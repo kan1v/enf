9880deb7133c0e552c3a24d0b88fc36a6a38c41a
Danylo <apruser.2016@gmail.com>
2025-08-21 14:01:14 +0300

    add app cart and functions

--- enf/settings.py
+++ enf/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'cart',
 
 ]
 
@@ -41,6 +42,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+    'cart.middleware.CartMiddleware',
 ]
 
 ROOT_URLCONF = 'enf.urls'
@@ -55,6 +58,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
+                'cart.context_processors.cart_processors',
             ],
         },
     },
@@ -123,3 +128,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SESSION_COOKIE_AGE = 86400 # 30 days
+SESSION_SAVE_EVERY_REQUEST = True
--- enf/urls.py
+++ enf/urls.py
@@ -5,7 +5,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cart/', include('cart.urls', namespace='cart')),
     path('', include(('main.urls', 'main'), namespace='main')),
+
 
 ]
 
